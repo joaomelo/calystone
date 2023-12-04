7d3e1970e49bbd5c67d627fd96dcf861a74c4885
joão melo <j@melo.plus>
2023-12-04 19:30:08 -0300

    feat: preferences draft

--- view/pages/page-preferences/messages.js
+++ view/pages/page-preferences/messages.js
@@ -1,8 +1,12 @@
 export const pagePreferences = {
   en: {
     preferences: "preferences",
+    locale: "locale",
+    done: "done",
   },
   "pt-br": {
     preferences: "preferências",
+    locale: "idioma",
+    done: "pronto",
   },
 };
--- view/pages/page-preferences/page-preferences.vue
+++ view/pages/page-preferences/page-preferences.vue
@@ -1,26 +1,29 @@
 <script setup>
-import { useI18n } from "@lib";
+import { useI18n, FormBase, InputBase, ButtonBase } from "@lib";
+import { useDisplay } from "@view/display";
 import { FrameDashboard } from "@view/frames";
 
-const { t } = useI18n();
-// const handleUpdate = (locale) => i18n.updateLocale(locale);
+const { t, i18n } = useI18n();
+const display = useDisplay();
+
+const handleI18n = (locale) => i18n.updateLocale(locale);
+const handleDone = () => display.pageOutline();
 </script>
 <template>
   <frame-dashboard :title="t('page-preferences.preferences')">
-    preferences
-    <!-- <form-base :error="edit.error" @submit="edit.run">
+    <form-base>
       <template #default>
         <input-base
-          :options="i18n.supported"
-          type="select"
+          :label="t('page-preferences.locale')"
           :model-value="i18n.locale"
-          @update:model-value="handleUpdate"
+          type="select"
+          :options="i18n.supported"
+          @update:model-value="handleI18n"
         />
       </template>
       <template #buttons>
-        <button-base type="submit" :label="t('save')" :busy="edit.busy" />
-        <button-base :label="t('cancel')" @click="push" />
+        <button-base :label="t('page-preferences.done')" @click="handleDone" />
       </template>
-    </form-base> -->
+    </form-base>
   </frame-dashboard>
 </template>
