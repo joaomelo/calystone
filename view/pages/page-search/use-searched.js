3602a65d196e107086ff4bc730bd168b3cb8e04e
joão melo <j@melo.plus>
2023-12-24 16:46:25 -0300

    feat: search show notes as tooltips

--- view/pages/page-search/use-searched.js
+++ view/pages/page-search/use-searched.js
@@ -1,14 +1,15 @@
 import { computed } from "vue";
-import { useDependencies } from "@lib";
+import { useDependencies, truncate } from "@lib";
 import { searchArtifacts } from "@body";
 
 export function useSearched(actions, term) {
   const dependencies = useDependencies();
   const list = computed(() => {
     const searched = searchArtifacts(dependencies, term.value);
-    return searched.map(({ id, name }) => ({
+    return searched.map(({ id, name, notes }) => ({
       value: id,
       text: name,
+      tooltip: truncate(notes, 100),
       actions,
     }));
   });
