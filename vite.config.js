12847f01d7d8ce01dea128a05659b11a2d070141
joão melo <j@melo.plus>
2023-12-25 17:06:16 -0300

    chore: local e2e test environment set to firebase emulators

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(() => {
     publicDir: "../assets",
     envDir: "../",
     server: {
-      port: 8080,
+      port: 8081,
       strictPort: true,
     },
     resolve: {
