ce6776332b7c42c7c732b1faaf6379a64739514b
joão melo <j@melo.plus>
2023-12-25 16:35:10 -0300

    chore: local development environment set to firebase emulators

--- main/main.js
+++ main/main.js
@@ -25,8 +25,8 @@ export function initApp(elementId) {
 
   const connection = createConnectionFromEnv();
   const driver = new FirebaseDriver(connection);
-  const auth = new FirebaseAuth(driver.app);
-  const selector = new FirestoreSelector(driver.app);
+  const auth = new FirebaseAuth(driver.auth);
+  const selector = new FirestoreSelector(driver.firestore);
   const mutator = new FirestoreMutator(driver.firestore);
 
   const dependencies = new Dependencies({
@@ -43,27 +43,41 @@ export function initApp(elementId) {
 }
 
 function createConnectionFromEnv() {
-  if (
-    typeof import.meta.env.VITE_API_KEY !== "string" ||
-    typeof import.meta.env.VITE_AUTH_DOMAIN !== "string" ||
-    typeof import.meta.env.VITE_PROJECT_ID !== "string" ||
-    typeof import.meta.env.VITE_STORAGE_BUCKET !== "string" ||
-    typeof import.meta.env.VITE_MESSAGING_SENDER_ID !== "string" ||
-    typeof import.meta.env.VITE_APP_ID !== "string" ||
-    typeof import.meta.env.VITE_MEASUREMENT_ID !== "string"
-  ) {
-    throw new Error(
-      "One or more of the environment variables for the service firebasedriver connection where not loaded properly"
-    );
+  if (import.meta.env.VITE_SERVER_PLATFORM === "emulators") {
+    console.info("using firebase emulators");
+    if (
+      typeof import.meta.env.VITE_PROJECT_ID !== "string" ||
+      typeof import.meta.env.VITE_API_KEY !== "string" ||
+      typeof import.meta.env.VITE_APP_ID !== "string"
+    ) {
+      throw new Error(
+        "One or more of the environment variables for the emulators connection where not loaded properly"
+      );
+    }
+  } else {
+    if (
+      typeof import.meta.env.VITE_PROJECT_ID !== "string" ||
+      typeof import.meta.env.VITE_API_KEY !== "string" ||
+      typeof import.meta.env.VITE_APP_ID !== "string" ||
+      typeof import.meta.env.VITE_AUTH_DOMAIN !== "string" ||
+      typeof import.meta.env.VITE_STORAGE_BUCKET !== "string" ||
+      typeof import.meta.env.VITE_MESSAGING_SENDER_ID !== "string" ||
+      typeof import.meta.env.VITE_MEASUREMENT_ID !== "string"
+    ) {
+      throw new Error(
+        "One or more of the environment variables for the service connection where not loaded properly"
+      );
+    }
   }
 
   return {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    serverPlatform: import.meta.env.VITE_SERVER_PLATFORM,
     projectId: import.meta.env.VITE_PROJECT_ID,
+    apiKey: import.meta.env.VITE_API_KEY,
+    appId: import.meta.env.VITE_APP_ID,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
     storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID,
     measurementId: import.meta.env.VITE_MEASUREMENT_ID,
   };
 }
