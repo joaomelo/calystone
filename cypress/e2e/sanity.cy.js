a276a9d48f7e9851354eeecc767f87c11e9ad01b
joão melo <j@melo.plus>
2023-12-26 16:23:39 -0300

    test: sanity test go basic

--- cypress/e2e/sanity.cy.js
+++ cypress/e2e/sanity.cy.js
@@ -1,9 +1,6 @@
 describe("sanity", () => {
-  it("visit active auth page", () => {
+  it("visit initial page", () => {
     cy.visit("/");
-    cy.get("#input-email input").type("test@test.com");
-    cy.get("#input-password input").type("1234567890");
-    cy.get("#button-sign-in").click();
-    cy.contains("user-not-found");
+    cy.contains("calystone");
   });
 });
