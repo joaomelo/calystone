6b730a19dd92bf1b2bd336e89df9cc5b2c509278
joão melo <j@melo.plus>
2024-01-12 11:43:18 -0300

    wip: chips aligned to the design system

--- lib/components/chip-base/chip-base.vue
+++ lib/components/chip-base/chip-base.vue
@@ -8,7 +8,20 @@ defineProps({
 </script>
 
 <template>
-  <div>
+  <div class="chip-base">
     {{ text }}
   </div>
 </template>
+
+<style scoped>
+
+.chip-base {
+  border: var(--size-00) solid var(--color-surface-40);
+  padding-inline: var(--size-05);
+  padding-block: var(--size-05);
+
+  font-size: var(--font-size-10);
+
+  background-color: var(--color-surface-20);
+}
+</style>
