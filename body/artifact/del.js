99e9c1c6d78f985d9d67727cb7038ec6dfdcdb92
joão melo <j@melo.plus>
2024-01-16 20:23:48 -0300

    fix: when deleting an artifact it will remove its id from from tag's artifactsIds lists

--- body/artifact/del.js
+++ body/artifact/del.js
@@ -1,13 +1,21 @@
-import { listDescendants } from "@body";
+import { alignTagsForManifests, listDescendants } from "@body";
 import { mutate } from "@lib";
 
-export async function delArtifact(dependencies, idOrArtifact) {
-  const subjectArtifacts = listDescendants(dependencies, idOrArtifact);
-  const manifests = subjectArtifacts.map(data => ({
-    data,
-    method: "del",
-    name: "artifacts",
-  }));
+export async function delArtifact(dependencies, artifactId) {
+  const manifests = [];
+
+  const artifactsToDel = listDescendants(dependencies, artifactId);
+  artifactsToDel.forEach(({ id }) => {
+    // must remove artifact from any tags its is associated with
+    const payload = { artifactId: id, tagsIds: [] };
+    const tagsManifests = alignTagsForManifests(dependencies, payload);
+    manifests.push(...tagsManifests);
+
+    manifests.push({ data: id, method: "del", name: "artifacts" });
+  });
+
+  console.log({ artifactsToDel, manifests });
+
   const { mutator } = dependencies;
   return mutate(mutator, manifests);
 }
--- body/tags/align.js
+++ body/tags/align.js
@@ -1,12 +1,18 @@
-import { mutate } from "@lib";
+import { arrayAdd, arrayDel, mutate } from "@lib";
 
 import { listTags, listTagsOf } from "./list";
 
 export function alignTagsFor(dependencies, { artifactId, tagsIds }) {
+  const manifests = alignTagsForManifests(dependencies, { artifactId, tagsIds });
+  const { mutator } = dependencies;
+  return mutate(mutator, manifests);
+}
+
+export function alignTagsForManifests(dependencies, { artifactId, tagsIds }) {
   const allTags = listTags(dependencies);
 
   const newTags = allTags.filter(({ id }) => tagsIds.includes(id));
-  const newTagsIds = newTags.map(({ id }) => id);
+  const newTagsIds = tagsIds;
 
   const currentTags = listTagsOf(dependencies, artifactId);
   const currentTagsIds = currentTags.map(({ id }) => id);
@@ -19,13 +25,12 @@ export function alignTagsFor(dependencies, { artifactId, tagsIds }) {
       name: "tags",
     });
 
+  const { mutator } = dependencies;
   currentTags.forEach((currentTag) => {
     // if this current tag of the artifact is not present in the list of new tags for the artifact then it should be updated.
     if (!newTagsIds.includes(currentTag.id)) {
       // the tag will need a manifest that updates its artifacts to exclude the one in case
-      const artifactsIds = Array.isArray(currentTag.artifactsIds)
-        ? currentTag.artifactsIds.filter(id => id !== artifactId)
-        : [];
+      const artifactsIds = arrayDel(mutator, artifactId);
       pushToManifest(currentTag.id, artifactsIds);
     }
   });
@@ -34,13 +39,10 @@ export function alignTagsFor(dependencies, { artifactId, tagsIds }) {
     // if the new tag for the artifact is not already present in the current list of tags linked to the artifact it should be updated
     if (!currentTagsIds.includes(newTag.id)) {
       // the tag will need a manifest that updates its artifacts to include the one in case
-      const artifactsIds = Array.isArray(newTag.artifactsIds)
-        ? newTag.artifactsIds.concat(artifactId)
-        : [artifactId];
+      const artifactsIds = arrayAdd(mutator, artifactId);
       pushToManifest(newTag.id, artifactsIds);
     }
   });
 
-  const { mutator } = dependencies;
-  return mutate(mutator, manifests);
+  return manifests;
 }
