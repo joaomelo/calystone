3fcbffede90a87e6371ee85cd4e0176dfe988c0c
joao melo <j@melo.plus>
2024-11-09 15:07:25 -0300

    refactor: split codemirror logic from artifact manipulation

---
+++ app/display/widgets/editor-text/code-mirror.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import { autocompletion, closeBrackets } from "@codemirror/autocomplete";
+import { defaultKeymap, history, historyKeymap } from "@codemirror/commands";
+import { markdown } from "@codemirror/lang-markdown";
+import {
+  bracketMatching,
+  defaultHighlightStyle,
+  indentOnInput,
+  syntaxHighlighting,
+} from "@codemirror/language";
+import {
+  highlightSelectionMatches,
+} from "@codemirror/search";
+import { EditorState } from "@codemirror/state";
+import {
+  drawSelection,
+  EditorView,
+  highlightActiveLine,
+  highlightActiveLineGutter,
+  keymap,
+  lineNumbers,
+} from "@codemirror/view";
+import { onMounted, onUnmounted, useTemplateRef, watch } from "vue";
+
+const model = defineModel<string>();
+
+const codeMirrorElement = useTemplateRef("codeMirror");
+let editorView: EditorView;
+
+onMounted(() => {
+  if (!codeMirrorElement.value) return;
+  const initialState = EditorState.create({
+    doc: model.value,
+    extensions: [
+      lineNumbers(),
+      EditorView.lineWrapping,
+      highlightActiveLineGutter(),
+      history(),
+      drawSelection(),
+      EditorState.allowMultipleSelections.of(true),
+      indentOnInput(),
+      syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+      bracketMatching(),
+      closeBrackets(),
+      autocompletion(),
+      highlightActiveLine(),
+      highlightSelectionMatches(),
+      keymap.of([...defaultKeymap, ...historyKeymap]),
+      markdown(),
+      EditorView.updateListener.of((v) => {
+        if (v.docChanged) {
+          model.value = editorView.state.doc.toString();
+        }
+      }),
+    ],
+  });
+
+  editorView = new EditorView({
+    parent: codeMirrorElement.value,
+    state: initialState,
+  });
+});
+
+onUnmounted(() => {
+  editorView.destroy();
+});
+
+watch(
+  () => model.value,
+  (newContent) => {
+    if (newContent === editorView.state.doc.toString()) return;
+    const update = editorView.state.update({
+      changes: {
+        from: 0,
+        insert: newContent,
+        to: editorView.state.doc.length,
+      },
+    });
+    editorView.update([update]);
+  }
+);
+</script>
+<template>
+  <div
+    ref="codeMirror"
+    class="code-mirror"
+  />
+</template>
+<style scoped>
+.code-mirror {
+  max-height: 100%;
+  min-height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+}
+
+.code-mirror :deep(.cm-editor) {
+  flex-grow: 1;
+}
+
+.code-mirror :deep(.cm-focused) {
+  outline: none;
+}
+</style>
--- app/display/widgets/editor-text/editor-text.vue
+++ app/display/widgets/editor-text/editor-text.vue
@@ -2,97 +2,27 @@
 import type { FileText } from "@/domain";
 
 import { fetchText } from "@/domain";
-import { autocompletion, closeBrackets } from "@codemirror/autocomplete";
-import { defaultKeymap, history, historyKeymap } from "@codemirror/commands";
-import { markdown } from "@codemirror/lang-markdown";
-import {
-  bracketMatching,
-  defaultHighlightStyle,
-  indentOnInput,
-  syntaxHighlighting,
-} from "@codemirror/language";
-import {
-  highlightSelectionMatches,
-} from "@codemirror/search";
-import { EditorState } from "@codemirror/state";
-import {
-  drawSelection,
-  EditorView,
-  highlightActiveLine,
-  highlightActiveLineGutter,
-  keymap,
-  lineNumbers,
-} from "@codemirror/view";
-import { onMounted, onUnmounted, ref, useTemplateRef } from "vue";
+import { onMounted, ref } from "vue";
+
+import CodeMirror from "./code-mirror.vue";
 
 const { artifact } = defineProps<{
   artifact: FileText;
 }>();
 
-const editorTextElement = useTemplateRef("editorText");
 const text = ref("");
-let editorView: EditorView;
-
 onMounted(async () => {
-  if (!editorTextElement.value) return;
-
   text.value = await fetchText(artifact);
-
-  const initialState = EditorState.create({
-    doc: text.value,
-    extensions: [
-      lineNumbers(),
-      EditorView.lineWrapping,
-      highlightActiveLineGutter(),
-      history(),
-      drawSelection(),
-      EditorState.allowMultipleSelections.of(true),
-      indentOnInput(),
-      syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
-      bracketMatching(),
-      closeBrackets(),
-      autocompletion(),
-      highlightActiveLine(),
-      highlightSelectionMatches(),
-      keymap.of([...defaultKeymap, ...historyKeymap]),
-      markdown(),
-      EditorView.updateListener.of((v) => {
-        if (v.docChanged) {
-          // emit("changed", editorView.state.doc.toString());
-        }
-      }),
-    ],
-  });
-
-  editorView = new EditorView({
-    parent: editorTextElement.value,
-    state: initialState,
-  });
 });
-onUnmounted(() => {
-  editorView.destroy();
-});
+
+function handleUpdate(value: string) {
+  // save to file
+  console.log(value);
+}
 </script>
 <template>
-  <div
-    ref="editorText"
-    class="editor-text"
+  <CodeMirror
+    :model-value="text"
+    @update:model-value="handleUpdate"
   />
 </template>
-<style scoped>
-.editor-text {
-  max-height: 100%;
-  min-height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: stretch;
-}
-
-.editor-text :deep(.cm-editor) {
-  flex-grow: 1;
-}
-
-.editor-text :deep(.cm-focused) {
-  outline: none;
-}
-</style>
