7ee1a015b75234da483e5ca89f79a321d93d68d6
joao melo <j@melo.plus>
2024-11-10 17:28:55 -0300

    refactor: file system access load updated to reflect resource api

--- app/domain/file-system-access/load/file.ts
+++ app/domain/file-system-access/load/file.ts
@@ -1,17 +1,19 @@
 import type { Id } from "@/utils";
 
-import { createArtifact, solveMime } from "@/domain/artifact";
+import { createArtifact } from "@/domain/artifact";
 
-import type { FileSystemAccessArtifact } from "../artifact";
+import type { FileSystemAccessArtifact, FileSystemAccessArtifactResources } from "../artifact";
 
 export async function loadFile(handle: FileSystemFileHandle, parentId?: Id): Promise<FileSystemAccessArtifact> {
   const file = await handle.getFile();
+  const resources: FileSystemAccessArtifactResources = { file, handle, source: "file-system-access" };
+
   const data = {
     lastModified: file.lastModified,
-    mime: solveMime(handle.name),
     name: file.name,
-    resources: { file, handle },
+    resources,
     size: file.size,
   };
+
   return createArtifact(data, parentId);
 }
