89cc49f1f3e6e0de0805eed6d84ab6adb2c081da
joao melo <j@melo.plus>
2024-12-26 19:03:08 -0300

    fix: import correct type

--- app/domain/nodes/nodes/repository.ts
+++ app/domain/nodes/nodes/repository.ts
@@ -1,11 +1,11 @@
 import type { ArtifactData } from "@/domain/nodes/artifact";
 import type { DirectoryData } from "@/domain/nodes/directory";
 import type { Id } from "@/domain/nodes/ids";
-import type { Matcher } from "@/domain/nodes/ignore";
+import type { Ignore } from "@/domain/nodes/ignore";
 
 export interface NodesRepository {
   fetchArtifactContent(id: Id): Promise<ArrayBuffer>;
-  loadNodesData(ignore: Matcher): AsyncGenerator<LoadNodeData>;
+  loadNodesData(ignore: Ignore): AsyncGenerator<LoadNodeData>;
   postArtifactContent(id: Id, content: ArrayBuffer): Promise<void>;
 }
 
