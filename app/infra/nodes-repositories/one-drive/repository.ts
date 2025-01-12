84fe611d14580a40acfb5c003ffb225a1dbff074
joao melo <j@melo.plus>
2025-01-12 10:51:20 -0300

    fix: correctly request onedrive file content

--- app/infra/nodes-repositories/one-drive/repository.ts
+++ app/infra/nodes-repositories/one-drive/repository.ts
@@ -1,7 +1,7 @@
 import type { ArtifactData, Id, NodeDataAndKind } from "@/domain";
 import type { DriveItem } from "@microsoft/microsoft-graph-types";
 
-import { Client, ResponseType } from "@microsoft/microsoft-graph-client";
+import { Client } from "@microsoft/microsoft-graph-client";
 
 import { BaseNodesRepository } from "../base";
 import { driveItemToNodeData } from "./item-to-node";
@@ -26,22 +26,34 @@ export class OneDriveNodesRepository extends BaseNodesRepository<undefined> {
   }
 
   async fetchArtifact(id: Id): Promise<ArtifactData> {
-    const fileMetadata = await this.graphClient
-      .api(`/me/drive/items/${id}`)
-      .select("size,lastModifiedDateTime")
-      .get() as { lastModifiedDateTime: string, size: number };
+    const fetchArtifactContent = async (id: Id) => {
+      // onedrive only returns the download url if the request is made exclusivy for it: https://stackoverflow.com/questions/57036249/search-query-doesnt-return-microsoft-graph-downloadurl
+      const downloadUrl = await this.graphClient
+        .api(`/me/drive/items/${id}`)
+        .select("@microsoft.graph.downloadUrl")
+        .get() as { "@microsoft.graph.downloadUrl": string };
+      const response = await fetch(downloadUrl["@microsoft.graph.downloadUrl"]);
+      const content = await response.arrayBuffer();
+      return { content };
+    };
 
-    const response = await this.graphClient
-      .api(`/me/drive/items/${id}/content`)
-      .responseType(ResponseType.ARRAYBUFFER)
-      .get() as { arrayBuffer: () => Promise<ArrayBuffer> };
+    const fetchArtifactMetadata = async (id: Id) => {
+      const fileMetadata = await this.graphClient
+        .api(`/me/drive/items/${id}`)
+        .select("size,lastModifiedDateTime")
+        .get() as { lastModifiedDateTime: string, size: number };
 
-    const arrayBuffer = await response.arrayBuffer();
+      return {
+        lastModified: new Date(fileMetadata.lastModifiedDateTime).getTime(),
+        size: fileMetadata.size
+      };
+    };
+
+    const data = await Promise.all([fetchArtifactContent(id), fetchArtifactMetadata(id)]);
 
     return {
-      content: arrayBuffer,
-      lastModified: new Date(fileMetadata.lastModifiedDateTime).getTime(),
-      size: fileMetadata.size
+      ...data[0],
+      ...data[1]
     };
   }
 
