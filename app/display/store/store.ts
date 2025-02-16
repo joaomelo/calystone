c76349c09b03e47c254874b5607e7d2153fa1d83
joao melo <j@melo.plus>
2025-02-16 13:00:10 -0300

    refactor: store no more dependent of the configuration class

--- app/display/store/store.ts
+++ app/display/store/store.ts
@@ -1,6 +1,5 @@
 import type { Nodes } from "@/domain";
 import type { NodesService } from "@/infra";
-import type { Configuration } from "@/utils";
 import type { App, ComputedRef } from "vue";
 
 import { computed } from "vue";
@@ -11,7 +10,6 @@ import { key } from "./key";
 import { useStore } from "./use";
 
 interface Options {
-  configuration: Configuration;
   nodes: Nodes;
   nodesService: NodesService
   appData: AppData;
@@ -19,13 +17,11 @@ interface Options {
 
 export class Store {
   appData: AppData;
-  configuration: Configuration;
   connected: ComputedRef<boolean>;
   nodes: Nodes;
   nodesService: NodesService;
 
-  constructor({ appData, configuration, nodes, nodesService }: Options) {
-    this.configuration = configuration;
+  constructor({ appData, nodes, nodesService }: Options) {
     this.nodes = nodes;
     this.connected = computed(() => this.nodes.hash.size > 0); // hash is manually set as a reactive property in the nodes class. been so, is the most appropriate property do check for connection status instead of the presence or not of a repository in the nodes instance.
     this.nodesService = nodesService;
