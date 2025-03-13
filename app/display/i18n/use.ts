0b5d73c873f98b534e01c983d6951bcb38502ec6
joao melo <j@melo.plus>
2025-03-13 17:59:03 -0300

    feat: erros are now more compatible with i18n

--- app/display/i18n/use.ts
+++ app/display/i18n/use.ts
@@ -1,11 +1,32 @@
+import { Exception } from "@/utils";
 import { useI18n as useVueI18n } from "vue-i18n";
 
 import type { Locale } from "./locales";
 import type { MessageSchema } from "./messages";
 
 export function useI18n() {
-  return useVueI18n<{ message: MessageSchema }, Locale>({
+  const utils = useVueI18n<{ message: MessageSchema }, Locale>({
     inheritLocale: true,
     useScope: "global"
   });
+
+  const tError = (error: unknown) => {
+    const baseKey = typeof error === "string"
+      ? error
+      : (error instanceof Exception)
+        ? error.code
+        : (error instanceof Error)
+          ? error.message
+          : "UNKNOWN_ERROR";
+
+    const message = utils.te(baseKey)
+      ? utils.t(baseKey)
+      : utils.te(`errors.${baseKey}`)
+        ? utils.t(`errors.${baseKey}`)
+        : baseKey;
+
+    return message;
+  };
+
+  return { ...utils, tError };
 }
--- app/utils/exception/exception.ts
+++ app/utils/exception/exception.ts
@@ -1,15 +1,15 @@
 import { Severity } from "./severities";
 
 export class Exception extends Error {
-  code;
-  detail;
-  severity;
+  readonly code;
+  readonly detail;
+  readonly severity;
 
   constructor(code: string, cause: unknown, severity?: Severity) {
     super(code);
     this.code = code;
     this.severity = severity ?? Severity.Error;
-    this.detail = JSON.stringify(cause);
+    this.detail = typeof cause === "string" ? cause : undefined;
     this.cause = cause;
     Error.captureStackTrace(this, Exception);
   }
