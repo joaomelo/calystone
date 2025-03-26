a5fc177fe731210cbb3b84bbe3ecb2e453af8ea1
joao melo <j@melo.plus>
2025-03-26 17:47:06 -0300

    chore: basic visual setup for node dragging

--- app/display/widgets/outline-nodes/outline-node.vue
+++ app/display/widgets/outline-nodes/outline-node.vue
@@ -1,16 +1,59 @@
 <script setup lang="ts">
 import type { Node } from "@/domain";
 
+import { Store } from "@/display/store";
+import { Directory } from "@/domain";
+import { useDispatch } from "@/utils";
+import { computed } from "vue";
+
 import { solveIcon } from "./solve-icon";
 
-defineProps<{
+const { node } = defineProps<{
   node: Node
 }>();
+const { nodes, service } = Store.use();
+
+const isMoveable = computed(() => service.nodeMove.support(node));
+const { dispatchOrToast } = useDispatch();
+const dragFormat = "application/node-id";
+
+async function handleDragdrop(event: DragEvent) {
+  if (!event.dataTransfer) return;
+
+  event.preventDefault();
+
+  const id = event.dataTransfer.getData(dragFormat);
+  if (id === node.id) return;
+  if (!(node instanceof Directory)) return;
+
+  await dispatchOrToast(async () => {
+    const subject = nodes.getOrThrow(id);
+    await service.nodeMove.move({ subject, target: node });
+  });
+}
+
+function handleDragover(event: DragEvent) {
+  if (!(node instanceof Directory)) return;
+  if (!event.dataTransfer?.types.includes(dragFormat)) return;
+  event.preventDefault();
+  event.dataTransfer.dropEffect = "move";
+}
+
+function handleDragstart(event: DragEvent) {
+  if (!event.dataTransfer) return;
+  event.dataTransfer.setData(dragFormat, node.id);
+  event.dataTransfer.effectAllowed = "move";
+}
 </script>
 <template>
   <div
-    draggable="false"
+    :draggable="isMoveable"
     class="outline-node"
+    :data-test="`outline-node-${node.id}`"
+    @dragstart="handleDragstart"
+    @drop="handleDragdrop"
+    @dragover="handleDragover"
+    @dragenter="handleDragover"
   >
     <i :class="solveIcon(node)" />
     <p>{{ node.name }}</p>
