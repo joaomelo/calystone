300064d910ed655b2fd7d5fb03d0f12bf0dce016
joao melo <j@melo.plus>
2025-03-30 10:01:28 -0300

    test: move to descendant node

--- e2e/specs/move.cy.ts
+++ e2e/specs/move.cy.ts
@@ -44,4 +44,49 @@ describe("move", () => {
           .should("exist");
       });
   });
+
+  it.only("cant move to descendant", () => {
+    outline
+      .directoryOf(outline.rootNode()).eq(0)
+      .as("sourceNodeWrapper");
+
+    outline
+      .inlineNodeOf(cy.get("@sourceNodeWrapper")).eq(0)
+      .as("sourceNode");
+
+    outline
+      .toogleOf(cy.get("@sourceNodeWrapper"))
+      .click();
+
+    outline
+      .childrenOf(cy.get("@sourceNodeWrapper")).eq(0)
+      .as("targetNodeWrapper");
+
+    outline
+      .inlineNodeOf(cy.get("@targetNodeWrapper"))
+      .as("targetNode");
+
+    cy.get("@sourceNode")
+      .invoke("attr", "data-test")
+      .then(dataTestString => {
+        if (typeof dataTestString !== "string") throw new Error("dataTestString is not a string");
+
+        const dataTransfer = new DataTransfer();
+        dataTransfer.setData("application/node-id", dataTestString);
+
+        cy.get("@sourceNode")
+          .trigger("dragstart", { dataTransfer })
+          .trigger("drag");
+
+        cy.get("@targetNode")
+          .trigger("dragover", { dataTransfer })
+          .trigger("drop", { dataTransfer });
+
+        cy.get("@sourceNode")
+          .should("exist");
+
+        cy.get("@targetNode")
+          .should("exist");
+      });
+  });
 });
