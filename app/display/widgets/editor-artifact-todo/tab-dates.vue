1c4927d8a0570eeb36c16beb921a567517e39c44
joao melo <j@melo.plus>
2025-04-25 20:06:55 -0300

    ui: layout adjustments for dates tab

--- app/display/widgets/editor-artifact-todo/tab-dates.vue
+++ app/display/widgets/editor-artifact-todo/tab-dates.vue
@@ -2,7 +2,7 @@
 import type { TodoArtifact } from "@/domain";
 
 import { Store } from "@/display/store";
-import { FieldSet, InputCheck, InputDate, useI18n } from "@/utils";
+import { InputCheck, InputDate, useI18n } from "@/utils";
 import { ref } from "vue";
 
 const { artifact } = defineProps<{
@@ -15,46 +15,44 @@ const { t } = useI18n();
 const allDay = ref(true);
 
 async function handleUpdateDueDate(due: Date | null | undefined) {
-  artifact.updateDueDate({ anchor: allDay.value, date: due ?? null });
+  artifact.updateDueDate({ anchor: allDay.value, date: due ?? undefined });
   await services.exchangeArtifact.postFrom(artifact);
 }
 
 async function handleUpdateStartDate(start: Date | null | undefined) {
-  artifact.updateStartDate({ anchor: allDay.value, date: start ?? null });
+  artifact.updateStartDate({ anchor: allDay.value, date: start ?? undefined });
   await services.exchangeArtifact.postFrom(artifact);
 }
 </script>
 <template>
-  <FieldSet :legend="t('dates.dates')">
-    <div class="dates-panel__inputs">
-      <InputDate
-        :label="t('dates.start')"
-        data-test="input-start"
-        :model-value="artifact.startDate()"
-        default-time="0:0"
-        :show-time="!allDay"
-        @update:model-value="handleUpdateStartDate"
-      />
-      <InputDate
-        :label="t('dates.due')"
-        data-test="input-due"
-        :model-value="artifact.dueDate()"
-        default-time="23:59"
-        :show-time="!allDay"
-        @update:model-value="handleUpdateDueDate"
-      />
-      <InputCheck
-        v-model="allDay"
-        :label="t('dates.allDay')"
-        data-test="input-all-day"
-      />
-    </div>
-  </FieldSet>
+  <div class="tab-dates">
+    <InputCheck
+      v-model="allDay"
+      :label="t('dates.allDay')"
+      data-test="input-all-day"
+    />
+    <InputDate
+      :label="t('dates.start')"
+      data-test="input-start"
+      :model-value="artifact.startDate()"
+      default-time="0:0"
+      :show-time="!allDay"
+      @update:model-value="handleUpdateStartDate"
+    />
+    <InputDate
+      :label="t('dates.due')"
+      data-test="input-due"
+      :model-value="artifact.dueDate()"
+      default-time="23:59"
+      :show-time="!allDay"
+      @update:model-value="handleUpdateDueDate"
+    />
+  </div>
 </template>
 <style scoped>
-.dates-panel__inputs {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
+.tab-dates {
+  display: flex;
+  flex-direction: column;
   gap: var(--size-3);
 }
 </style>
