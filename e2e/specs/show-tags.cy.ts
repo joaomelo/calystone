971d0154e58cf360d9086a25ca8c5deeddc08ac6
joao melo <j@melo.plus>
2025-05-01 09:57:44 -0300

    wip: draft test case for tag view

---
+++ e2e/specs/show-tags.cy.ts
@@ -0,0 +1,15 @@
+import { outline, pageOpen } from "../helpers";
+
+describe("show tags", () => {
+  beforeEach(() => {
+    pageOpen.macros.openMemory();
+  });
+
+  it("show tags based on todos", () => {
+    // create a todo with two tags and high priority
+    // create a todo with a shared tag and a unique tag and low priority
+    // go to tags view
+    // check that all three tags are present and the respective todos are shown in proper order
+  });
+
+});
