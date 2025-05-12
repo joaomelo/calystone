da8ee322af6b99e572669e16241d0e965e840b7a
joao melo <j@melo.plus>
2025-05-12 18:07:45 -0300

    fix: wrong import

--- app/domain/artifact/todo/state.ts
+++ app/domain/artifact/todo/state.ts
@@ -1,6 +1,6 @@
+import type { Dater } from "@/domain/artifact/dater";
 import type { Recurrer } from "@/domain/artifact/recurrer";
 
-import type { Dater } from "./dater";
 import type { Prioritizer } from "./prioritizer";
 import type { Progressor } from "./progressor";
 import type { Tagger } from "./tagger";
