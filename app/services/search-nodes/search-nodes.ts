bca8a6eaf1fecbecaf70a64af1f059f87dc11fab
joao melo <j@melo.plus>
2025-05-18 12:23:46 -0300

    chore: search does fuzzy comparison at nodes names

--- app/services/search-nodes/search-nodes.ts
+++ app/services/search-nodes/search-nodes.ts
@@ -1,15 +1,32 @@
-import type { Nodes } from "@/domain";
+import type { Node, Nodes } from "@/domain";
+
+import Fuse from "fuse.js";
 
 export class SearchNodesService {
+  private readonly fuseOptions = {
+    ignoreLocation: true,
+    includeScore: true,
+    keys: ["name"],
+    shouldSort: true,
+    threshold: 0.3,
+    useExtendedSearch: true
+  };
   private nodes: Nodes;
 
   constructor(nodes: Nodes) {
     this.nodes = nodes;
   }
 
-  search(text: string) {
-    const neutralizedText = text.toLowerCase().trim();
-    const results = this.nodes.list().filter(node => node.name.toLowerCase().includes(neutralizedText));
-    return results;
+  search(options: { limit?: number; text: string, }): Node[] {
+    const { limit = 100, text } = options;
+
+    if (!text.trim()) {
+      return [];
+    }
+
+    const fuse = new Fuse(this.nodes.list(), this.fuseOptions);
+    const results = fuse.search(text);
+
+    return results.map(result => result.item).slice(0, limit);
   }
 }
