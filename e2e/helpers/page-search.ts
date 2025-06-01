1a4323135611a3f68733fce37ee786c1c894e35c
joao melo <j@melo.plus>
2025-06-01 12:16:46 -0300

    tests: proper selector for input search

--- e2e/helpers/page-search.ts
+++ e2e/helpers/page-search.ts
@@ -2,7 +2,7 @@ import { dataTest } from "./data-test";
 import { frameDashboard } from "./frame-dashboard";
 
 export const pageSearch = {
-  input: () => cy.get(dataTest("input-search")),
+  input: () => cy.get(dataTest("outline-search__input")),
   resultItem: (name: string) => cy.get("li").contains(name),
   results: () => cy.get(dataTest("search-results")),
   url: () => "/search",
