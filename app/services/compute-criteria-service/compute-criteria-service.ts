3bb15cb38a0d220dd18bcf58273142888c2b9a34
joao melo <j@melo.plus>
2025-07-12 10:59:08 -0300

    wip: service to list criterion labels

---
+++ app/services/compute-criteria-service/compute-criteria-service.ts
@@ -0,0 +1,31 @@
+import type { Tag } from "@/domain";
+import type { RetrieveNodesService } from "@/services/retrieve-nodes-service";
+
+import { Tags, TodoArtifact } from "@/domain";
+import { throwError } from "@/utils";
+
+export class ComputeTagsService {
+  private readonly retrieveNodesService: RetrieveNodesService;
+
+  constructor(retrieveNodesService: RetrieveNodesService) {
+    this.retrieveNodesService = retrieveNodesService;
+  }
+
+  compute(): Tags {
+    const tags = new Tags();
+    const nodes = this.retrieveNodesService.list();
+    for (const node of nodes) {
+      if (node instanceof TodoArtifact) {
+        tags.add(node);
+      }
+    }
+    return tags;
+  }
+
+  getTagOrThrow(name: string): Tag {
+    const tags = this.compute();
+    const tag = tags.get(name);
+    if (!tag) throwError("TAG_NOT_FOUND");
+    return tag;
+  }
+}
