fc731f5659992e6fc88f9969c43059da63490c17
joao melo <j@melo.plus>
2025-07-16 17:57:55 -0300

    tests: remove test from removed feature

--- e2e/helpers/frame-dashboard.ts
+++ e2e/helpers/frame-dashboard.ts
@@ -4,7 +4,6 @@ export const frameDashboard = {
   calendar: () => cy.get(dataTest("sidebar-calendar")),
   exit: () => cy.get(dataTest("sidebar-exit")),
   preloading: () => cy.get(dataTest("sidebar-preloading")),
-  reload: () => cy.get(dataTest("sidebar-reload")),
   search: () => cy.get(dataTest("sidebar-search")),
   tags: () => cy.get(dataTest("sidebar-tags")),
 } as const;
