206cc72172f5619a9377fac8fe9cbe4033fe529b
joao melo <j@melo.plus>
2025-07-19 13:10:50 -0300

    refactor: input number composes subcomponents instead of using wrapper

--- app/utils/inputs/input-number/input-number.vue
+++ app/utils/inputs/input-number/input-number.vue
@@ -1,45 +1,63 @@
 <script setup lang="ts">
+import { kebabCase } from "@/utils/text";
 import InputNumberPrimeVue from "primevue/inputnumber";
+import { useId, } from "vue";
 
-import { InputWrapper } from "../input-wrapper";
+import { InputLabel } from "../input-label";
 
 const {
-  buttons = false, disabled = false, locale = "en", size
+  buttons = false,
+  disabled = false,
+  locale = "en",
 } = defineProps<{
   buttons?: boolean
   dataTest: string
   disabled?: boolean
+  label?: string
   locale?: string
   max?: number
   maxFractionDigits?: number
   min?: number
   minFractionDigits?: number
-  size?: "large" | "small"
   step?: number
 }>();
 const model = defineModel({ type: Number });
+
+const id = useId();
+
 </script>
 <template>
-  <InputWrapper :data-test="dataTest">
-    <template #default="{ id, invalid, inputDataTest }">
-      <InputNumberPrimeVue
-        :id="id"
-        v-model="model"
-        class="input-number"
-        :invalid="invalid"
-        :data-test="inputDataTest"
-        fluid
-        :locale="locale"
-        :show-buttons="buttons"
-        :max="max"
-        :min="min"
-        :step="step"
-        :size="size"
-        button-layout="horizontal"
-        :disabled="disabled"
-        :max-fraction-digits="maxFractionDigits"
-        :min-fraction-digits="minFractionDigits"
-      />
-    </template>
-  </InputWrapper>
+  <div
+    :data-test="dataTest"
+    class="input-number"
+  >
+    <InputLabel
+      v-if="label"
+      :label="label"
+      :data-test="kebabCase(dataTest, 'label')"
+      :for-id="id"
+    />
+    <InputNumberPrimeVue
+      :id="id"
+      v-model="model"
+      :data-test="kebabCase(dataTest, 'input')"
+      fluid
+      :locale="locale"
+      :show-buttons="buttons"
+      :max="max"
+      :min="min"
+      :step="step"
+      button-layout="horizontal"
+      :disabled="disabled"
+      :max-fraction-digits="maxFractionDigits"
+      :min-fraction-digits="minFractionDigits"
+    />
+  </div>
 </template>
+<style scoped>
+.input-number {
+  display: flex;
+  flex-direction: column;
+  gap: var(--size-2);
+}
+</style>
