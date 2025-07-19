fec0a5d99f84c162e3d46255cc40110668fb26c5
joao melo <j@melo.plus>
2025-07-19 14:15:28 -0300

    refactor: input text composes subcomponents instead of using wrapper

--- app/utils/inputs/input-text/input-text.vue
+++ app/utils/inputs/input-text/input-text.vue
@@ -2,13 +2,19 @@
 import { hasElements } from "@/utils/arrays";
 import { kebabCase } from "@/utils/text";
 import InputTextPrimeVue from "primevue/inputtext";
-import { computed } from "vue";
+import {
+  computed,
+  useId
+} from "vue";
 
-import { InputWrapper } from "../input-wrapper";
+import { InputError } from "../input-error";
+import { InputLabel } from "../input-label";
 
 const { suggestions = [] } = defineProps<{
   autofocus?: boolean
   dataTest: string
+  error?: string
+  label?: string;
   readonly?: boolean;
   suggestions?: string[]
 }>();
@@ -16,31 +22,51 @@ const model = defineModel<string>();
 
 const hasSuggestions = computed(() => hasElements(suggestions));
 
+const id = useId();
 const formDatalistName = (baseId: string) => kebabCase(baseId, "suggestions");
 </script>
 <template>
-  <InputWrapper :data-test="dataTest">
-    <template #default="{ id, invalid, inputDataTest }">
-      <InputTextPrimeVue
-        :id="id"
-        v-model="model"
-        :invalid="invalid"
-        :data-test="inputDataTest"
-        fluid
-        :autofocus="autofocus"
-        :list="formDatalistName(id)"
-        :readonly="readonly"
+  <div
+    :data-test="dataTest"
+    class="input-text"
+  >
+    <InputLabel
+      v-if="label"
+      :label="label"
+      :data-test="kebabCase(dataTest, 'label')"
+      :for-id="id"
+    />
+    <InputTextPrimeVue
+      :id="id"
+      v-model="model"
+      :invalid="!!error"
+      :data-test="kebabCase(dataTest, 'input')"
+      fluid
+      :autofocus="autofocus"
+      :list="formDatalistName(id)"
+      :readonly="readonly"
+    />
+    <datalist
+      v-if="hasSuggestions"
+      :id="formDatalistName(id)"
+    >
+      <option
+        v-for="suggestion in suggestions"
+        :key="suggestion"
+        :value="suggestion"
       />
-      <datalist
-        v-if="hasSuggestions"
-        :id="formDatalistName(id)"
-      >
-        <option
-          v-for="suggestion in suggestions"
-          :key="suggestion"
-          :value="suggestion"
-        />
-      </datalist>
-    </template>
-  </InputWrapper>
+    </datalist>
+    <InputError
+      v-if="error"
+      :message="error"
+      :data-test="kebabCase(dataTest, 'error')"
+    />
+  </div>
 </template>
+<style scoped>
+.input-radio {
+  display: flex;
+  flex-direction: column;
+  gap: var(--size-2);
+}
+</style>
