bb866164ad79078ead384fcd62efc7ac58e538dc
joao melo <j@melo.plus>
2025-08-27 17:47:22 -0300

    tests: reestablishes tests to the new naming pattern for property sheets components

--- e2e/selectors/editor-binary.ts
+++ e2e/selectors/editor-binary.ts
@@ -1,12 +1,12 @@
 import { dataTest } from "../helpers";
 
 export const editorBinary = {
-  lastModifiedLabel: () => cy.get(dataTest("property-sheet-3-label")),
-  lastModifiedValue: () => cy.get(dataTest("property-sheet-3-value")),
-  pathLabel: () => cy.get(dataTest("property-sheet-1-label")),
-  pathValue: () => cy.get(dataTest("property-sheet-1-value")),
-  sizeLabel: () => cy.get(dataTest("property-sheet-2-label")),
-  sizeValue: () => cy.get(dataTest("property-sheet-2-value")),
-  typeLabel: () => cy.get(dataTest("property-sheet-0-label")),
-  typeValue: () => cy.get(dataTest("property-sheet-0-value")),
+  lastModifiedLabel: () => cy.get(dataTest("property-sheet-label-last-modified")),
+  lastModifiedValue: () => cy.get(dataTest("property-sheet-value-last-modified")),
+  pathLabel: () => cy.get(dataTest("property-sheet-label-path")),
+  pathValue: () => cy.get(dataTest("property-sheet-value-path")),
+  sizeLabel: () => cy.get(dataTest("property-sheet-label-size")),
+  sizeValue: () => cy.get(dataTest("property-sheet-value-size")),
+  typeLabel: () => cy.get(dataTest("property-sheet-label-type")),
+  typeValue: () => cy.get(dataTest("property-sheet-value-type")),
 } as const;
--- e2e/selectors/editor-directory.ts
+++ e2e/selectors/editor-directory.ts
@@ -4,9 +4,9 @@ export const editorDirectory = {
   buttonReload: () => cy.get(dataTest("button-reload-directory")),
   descriptorContent: () => cy.get(dataTest("descriptor-content")),
   descriptorTip: () => cy.get(dataTest("descriptor-tip")),
-  itemsLabel: () => cy.get(dataTest("property-sheet-1-label")),
-  itemsValue: () => cy.get(dataTest("property-sheet-1-value")),
-  pathLabel: () => cy.get(dataTest("property-sheet-0-label")),
-  pathValue: () => cy.get(dataTest("property-sheet-0-value")),
+  itemsLabel: () => cy.get(dataTest("property-sheet-label-items")),
+  itemsValue: () => cy.get(dataTest("property-sheet-value-items")),
+  pathLabel: () => cy.get(dataTest("property-sheet-label-path")),
+  pathValue: () => cy.get(dataTest("property-sheet-value-path")),
   tipUnloaded: () => cy.get(dataTest("tip-unloaded")),
 } as const;
