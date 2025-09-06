2b188d529c2f0cdee0157d7a175f70a0f7c9d58a
joao melo <j@melo.plus>
2025-09-06 15:16:17 -0300

    ui: icon for outline text nodes

--- app/display/views/outlines-node/outline-text/outline-text.vue
+++ app/display/views/outlines-node/outline-text/outline-text.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import type { TextArtifact } from "@/domain";
 
-import { truncate } from "@/utils";
+import {
+  AppIcon,
+  truncate
+} from "@/utils";
 import { computed } from "vue";
 
 import { OutlineNode } from "../outline-node";
 
 const { text } = defineProps<{ text: TextArtifact; }>();
 
-const icon = computed(() => {
-  const iconPrefix = "bx bx-sm";
-  const loadingEffect = text.isBusy() ? "bx-flashing" : "";
-  const iconGlyph = text.isLoaded() ? "bxs-file-txt" : "bx-file-blank";
-  return `${iconPrefix} ${loadingEffect} ${iconGlyph}`;
+const animation = computed(() => {
+  return text.isBusy() ? "pulse" : "none";
+});
+
+const name = computed(() => {
+  return text.isLoaded() ? "file-text" : "file-dashed";
 });
 
 const excerpt = computed(() => {
@@ -27,7 +31,10 @@ const excerpt = computed(() => {
 <template>
   <OutlineNode :node="text">
     <template #icon>
-      <i :class="icon" />
+      <AppIcon
+        :name="name"
+        :animation="animation"
+      />
     </template>
     <template #meta>
       <div class="outline-text__excerpt">
